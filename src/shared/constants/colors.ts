a3f5146296dc34b552fa8dd289fe3195f2bdcfea
roshik14 <fraust1337@gmail.com>
2023-08-07 00:06:03 +0700

    feat: add colors constats for button component

---
+++ src/shared/constants/colors.ts
@@ -0,0 +1,6 @@
+export const PRIMARY_COLOR = "#f7d22d";
+export const CONTAINED_BUTTON_BG_COLOR = "#f7d22d";
+export const CONTAINED_BUTTON_BG_HOVER_COLOR = "#efcc00";
+export const TEXT_BUTTON_COLOR = "#696f7a";
+export const OUTLINED_BUTTON_COLOR = "#0e0c0d";
+export const OUTLINED_BUTTON_BORDER_COLOR = "#E3ECF5";
