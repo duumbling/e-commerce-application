d8fbbe6975615022f2b1e1c0c6bf08ef06e0498e
SkarabeyDM <mechanoid0415@gmail.com>
2023-08-07 13:32:26 +0300

    refactor: remove unnecessary return types

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { Button } from "@mui/material";
 
-function App(): JSX.Element {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
