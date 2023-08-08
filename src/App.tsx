8e089a271a14c9b6442a8ed7bcf88b64a2885ab9
SkarabeyDM <mechanoid0415@gmail.com>
2023-08-08 13:53:49 +0300

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import "./App.css";
-import { Button } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "./shared/ui/layout";
+import MainPage from "./pages/main";
+import LoginPage from "./pages/login";
+import RegisterPage from "./pages/register";
+import NotFoundPage from "./pages/notFound";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Button>Hello World</Button>
-      </header>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="register" element={<RegisterPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
