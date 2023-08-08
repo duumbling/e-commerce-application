272a30221ec27e7b736ae018a911d825ece32081
SkarabeyDM <mechanoid0415@gmail.com>
2023-08-08 20:54:30 +0300

    docs: add future task

--- src/shared/ui/layout.tsx
+++ src/shared/ui/layout.tsx
@@ -4,14 +4,13 @@ import { Link, Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <>
+      {/* TODO Заменить это нормальным хедером. */}
       <header>
         <Link to="/">Main</Link>
         <Link to="/login">Login</Link>
         <Link to="/register">Register</Link>
       </header>
-
       <Outlet />
-
       <footer>--Footer--</footer>
     </>
   );
