d9cebf33cfbde3ade9fcd72b6c30d7d42aa9e917
SkarabeyDM <mechanoid0415@gmail.com>
2023-08-08 13:52:02 +0300

    feat: add pages `login`, `main`, `register` and `notFound`

---
+++ src/pages/main/MainPage.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+export const MainPage = () => {
+  return <div>Main page</div>;
+};
---
+++ src/pages/notFound/NotFoundPage.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+export const NotFoundPage = () => {
+  return <div>404</div>;
+};
