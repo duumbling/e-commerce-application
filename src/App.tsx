64f24ff46ffc87a91e89f4a2a74da529829112dc
SkarabeyDM <mechanoid0415@gmail.com>
2023-08-10 13:55:40 +0300

    refactor: put the paths in a separate enum

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,17 @@ import MainPage from "./pages/main";
 import LoginPage from "./pages/login";
 import RegisterPage from "./pages/register";
 import NotFoundPage from "./pages/notFound";
+import { Paths } from "./shared/constants/paths";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path={Paths.Main} element={<Layout />}>
           <Route index element={<MainPage />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="register" element={<RegisterPage />} />
-          <Route path="*" element={<NotFoundPage />} />
+          <Route path={Paths.Login} element={<LoginPage />} />
+          <Route path={Paths.Register} element={<RegisterPage />} />
+          <Route path={Paths.NotFound} element={<NotFoundPage />} />
         </Route>
       </Routes>
     </div>
---
+++ src/shared/constants/paths.ts
@@ -0,0 +1,6 @@
+export const enum Paths {
+  Main = "/",
+  Login = "/login",
+  Register = "/register",
+  NotFound = "*",
+}
