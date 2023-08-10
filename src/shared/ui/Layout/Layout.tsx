84432c28302dce996b5f26f7543aae9c8bd91d68
SkarabeyDM <mechanoid0415@gmail.com>
2023-08-10 23:34:20 +0300

    refactor: remove `Header` from `Layout`

--- src/shared/ui/Layout/Layout.tsx
+++ src/shared/ui/Layout/Layout.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { Header } from "../Header";
 
 export function Layout() {
   return (
     <div className="wrapper">
-      <Header />
       <Outlet />
       <footer>--Footer--</footer>
     </div>
