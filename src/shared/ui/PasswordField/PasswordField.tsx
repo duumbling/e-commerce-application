201cb603ffb29b2a6d8a57715696dffccd927e25
roshik14 <fraust1337@gmail.com>
2023-08-11 20:09:28 +0700

    fix: change icon

--- src/shared/ui/PasswordField/PasswordField.tsx
+++ src/shared/ui/PasswordField/PasswordField.tsx
@@ -1,43 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, forwardRef } from "react";
 import { IconButton, InputAdornment, type TextFieldProps } from "@mui/material";
 import { CustomTextField } from "../CustomTextField";
-import { CustomVisibilityOffIcon } from "../CustomVisibilityOffIcon";
 import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
-export const PasswordField = ({
-  label,
-  sx,
-  InputProps,
-  ...props
-}: TextFieldProps) => {
-  const [isPasswordVisible, setPasswordVisibleState] = useState(false);
+export const PasswordField = forwardRef<HTMLDivElement, TextFieldProps>(
+  function PasswordField({ label, sx, InputProps, ...props }, ref) {
+    const [isPasswordVisible, setPasswordVisibleState] = useState(false);
 
-  return (
-    <CustomTextField
-      {...props}
-      type={isPasswordVisible ? "text" : "password"}
-      label={label}
-      sx={sx}
-      InputProps={{
-        endAdornment: (
-          <InputAdornment position="end">
-            <IconButton
-              aria-label="toggle password visibility"
-              onClick={() => {
-                setPasswordVisibleState(!isPasswordVisible);
-              }}
-              edge="end"
-            >
-              {isPasswordVisible ? (
-                <VisibilityOutlinedIcon />
-              ) : (
-                <CustomVisibilityOffIcon />
-              )}
-            </IconButton>
-          </InputAdornment>
-        ),
-        ...InputProps,
-      }}
-    />
-  );
-};
+    return (
+      <CustomTextField
+        {...props}
+        type={isPasswordVisible ? "text" : "password"}
+        label={label}
+        sx={sx}
+        inputRef={ref}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={() => {
+                  setPasswordVisibleState(!isPasswordVisible);
+                }}
+                edge="end"
+              >
+                {isPasswordVisible ? (
+                  <VisibilityOutlinedIcon />
+                ) : (
+                  <VisibilityOffIcon />
+                )}
+              </IconButton>
+            </InputAdornment>
+          ),
+          ...InputProps,
+        }}
+      />
+    );
+  },
+);
