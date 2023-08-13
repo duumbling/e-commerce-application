dc0ea6f33c1e0d4777939caca0bdb90e46371696
SkarabeyDM <mechanoid0415@gmail.com>
2023-08-13 13:36:59 +0300

    feat: add themed colors

--- src/shared/ui/Link/Link.tsx
+++ src/shared/ui/Link/Link.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { Link as MuiLink, type LinkProps as MuiLinkProps } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
-import type { Omit } from "@reduxjs/toolkit/dist/tsHelpers";
 import type { Paths } from "../../constants/paths";
+import { ThemeColors } from "../../constants/colors";
 
-type LinkProps = Omit<MuiLinkProps, "href"> & {
+type LinkProps = MuiLinkProps & {
   href: Paths | string;
+  color: ThemeColors;
 };
 
-export function Link({ href, color = "secondary", ...otherProps }: LinkProps) {
+export function Link({
+  href,
+  color = ThemeColors.GREY_DARKER,
+  ...otherProps
+}: LinkProps) {
   return (
     <MuiLink
       component={RouterLink}
