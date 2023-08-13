804a282784c46849662064ef9fa6c22df5163563
roshik14 <fraust1337@gmail.com>
2023-08-13 13:05:02 +0700

    feat: add useFormContext to AboutForm

--- src/features/AboutForm/ui/AboutForm.tsx
+++ src/features/AboutForm/ui/AboutForm.tsx
@@ -7,13 +7,14 @@ import {
   type FormLabelProps,
 } from "@mui/material";
 import { CustomTextField } from "../../../shared/ui/CustomTextField";
+import { useFormContext } from "react-hook-form";
 
 type AboutFormProps = Pick<BoxProps, "sx"> & {
   title: string;
   titleProps: FormLabelProps;
   firstNameFieldProps?: TextFieldProps;
   lastNameFieldProps?: TextFieldProps;
-  birthDateFieldProps?: TextFieldProps;
+  userBirthDayProps?: TextFieldProps;
   emailFieldProps?: TextFieldProps;
   isContainEmail?: boolean;
 };
@@ -24,26 +25,43 @@ export const AboutForm = ({
   sx,
   firstNameFieldProps,
   lastNameFieldProps,
-  birthDateFieldProps,
+  userBirthDayProps,
   emailFieldProps,
   isContainEmail,
 }: AboutFormProps) => {
+  const { register } = useFormContext();
   return (
     <Box>
       <FormLabel {...titleProps} sx={{ display: "block", ...titleProps?.sx }}>
         {title}
       </FormLabel>
       <Box display="flex" flexDirection="column" sx={sx}>
-        <CustomTextField label="Имя" type="text" {...firstNameFieldProps} />
-        <CustomTextField label="Фамилия" type="text" {...lastNameFieldProps} />
+        <CustomTextField
+          label="Имя"
+          type="text"
+          {...firstNameFieldProps}
+          {...register("firstName")}
+        />
+        <CustomTextField
+          label="Фамилия"
+          type="text"
+          {...lastNameFieldProps}
+          {...register("lastName")}
+        />
         <CustomTextField
           label="Дата рождения"
           type="date"
           InputLabelProps={{ shrink: true }}
-          {...birthDateFieldProps}
+          {...userBirthDayProps}
+          {...register("userBirthday")}
         />
         {isContainEmail ?? false ? (
-          <CustomTextField label="Email" type="email" {...emailFieldProps} />
+          <CustomTextField
+            label="Email"
+            type="email"
+            {...emailFieldProps}
+            {...register("email")}
+          />
         ) : null}
       </Box>
     </Box>
