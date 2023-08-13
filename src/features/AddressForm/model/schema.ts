c40df395e64956d21960e6b750572d9f3e60bf55
roshik14 <fraust1337@gmail.com>
2023-08-13 23:18:32 +0700

    feat: add validation for postal code

--- src/features/AddressForm/model/schema.ts
+++ src/features/AddressForm/model/schema.ts
@@ -1,9 +1,20 @@
 import { object, string } from "yup";
 import { matchOnlyAlphabeticCharacters } from "../../../shared/lib/validation";
 
+const POSTAL_CODE_LENGTH = 6;
+
 export const addressValidationSchema = object({
   country: string().required("Выберите страну"),
   city: matchOnlyAlphabeticCharacters(string().required("Введите город")),
   streetName: string().required("Введите название улицы"),
-  postalCode: string().required("Введите почтовый индекс"),
+  postalCode: string()
+    .required("Введите почтовый индекс")
+    .max(
+      POSTAL_CODE_LENGTH,
+      `Идекс должен содержать ${POSTAL_CODE_LENGTH} цифр`,
+    )
+    .min(
+      POSTAL_CODE_LENGTH,
+      `Идекс должен содержать ${POSTAL_CODE_LENGTH} цифр`,
+    ),
 });
