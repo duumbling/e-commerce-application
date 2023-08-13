352f1dd1f8ab975dcd8b126d7f548b486cce5303
SkarabeyDM <mechanoid0415@gmail.com>
2023-08-13 15:10:53 +0300

    fix: make `color` property optional

--- src/shared/ui/Link/Link.tsx
+++ src/shared/ui/Link/Link.tsx
@@ -3,10 +3,11 @@ import { Link as MuiLink, type LinkProps as MuiLinkProps } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
 import type { Paths } from "../../constants/paths";
 import { ThemeColors } from "../../constants/colors";
+import type { Omit } from "@reduxjs/toolkit/dist/tsHelpers";
 
-type LinkProps = MuiLinkProps & {
-  href: Paths | string;
-  color: ThemeColors;
+type LinkProps = Omit<MuiLinkProps, "color"> & {
+  href: Paths;
+  color?: ThemeColors;
 };
 
 export function Link({
