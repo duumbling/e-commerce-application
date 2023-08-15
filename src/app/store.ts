ca9fb534c9b749090c6b8414328fe7f51f4b14c2
roshik14 <fraust1337@gmail.com>
2023-08-15 13:17:15 +0700

    feat: add export and import addressReducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { addressReducer } from "../features/AddressForm";
 
 const myReducer = (): number => 1;
 
 export const store = configureStore({
   reducer: {
     myReducer,
+    addressReducer,
   },
 });
