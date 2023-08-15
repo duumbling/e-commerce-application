c83a477351ff7b798422cde8401a94aadc37455a
roshik14 <fraust1337@gmail.com>
2023-08-15 13:19:38 +0700

    feat: add store provider and delete standard cra css styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { Layout } from "./shared/ui/Layout";
 import { MainPage } from "./pages/main";
@@ -7,19 +6,23 @@ import { LoginPage } from "./pages/login";
 import { RegisterPage } from "./pages/register";
 import { NotFoundPage } from "./pages/notFound";
 import { Paths } from "./shared/constants/paths";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path={Paths.Main} element={<Layout />}>
-          <Route index element={<MainPage />} />
-          <Route path={Paths.Login} element={<LoginPage />} />
-          <Route path={Paths.Register} element={<RegisterPage />} />
-          <Route path={Paths.NotFound} element={<NotFoundPage />} />
-        </Route>
-      </Routes>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Routes>
+          <Route path={Paths.Main} element={<Layout />}>
+            <Route index element={<MainPage />} />
+            <Route path={Paths.Login} element={<LoginPage />} />
+            <Route path={Paths.Register} element={<RegisterPage />} />
+            <Route path={Paths.NotFound} element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </div>
+    </Provider>
   );
 }
 
