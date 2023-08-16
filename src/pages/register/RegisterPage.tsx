1bc75608827245225784b412b06caa6256f13647
roshik14 <fraust1337@gmail.com>
2023-08-16 09:49:13 +0700

    feat: add registration form on the page

--- src/pages/register/RegisterPage.tsx
+++ src/pages/register/RegisterPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Header } from "../../shared/ui/Header";
+import { RegistrationForm } from "../../widgets/registration-form";
 
 export function RegisterPage() {
   return (
     <div>
       <Header>Register page</Header>
+      <RegistrationForm />
     </div>
   );
 }
