d9a772778cc8b7922ee084fcfd3e7ca71b7f45fa
duumbling <123836489+duumbling@users.noreply.github.com>
2023-08-16 08:09:45 +0300

    fix: remove excess div

--- src/pages/notFound/ui/NotFoundPage.tsx
+++ src/pages/notFound/ui/NotFoundPage.tsx
@@ -15,8 +15,7 @@ import { useNavigate } from "react-router-dom";
 export function NotFoundPage() {
   const navigate = useNavigate();
   return (
-    <div>
-      <Box>
+     <Box>
         <Grid {...gridContainerProps}>
           <Grid {...gridHeaderProps}>
             <Header></Header>
@@ -42,6 +41,5 @@ export function NotFoundPage() {
           </Grid>
         </Grid>
       </Box>
-    </div>
   );
 }
