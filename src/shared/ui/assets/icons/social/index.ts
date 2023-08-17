950585fea2248900ebce5dca26badf2639de13b2
SkarabeyDM <mechanoid0415@gmail.com>
2023-08-17 20:58:43 +0300

    feat: add social icons export

---
+++ src/shared/ui/assets/icons/social/index.ts
@@ -0,0 +1,8 @@
+import iconFacebook from "./facebook_icon.png";
+import iconSignal from "./signal_icon.png";
+import iconSkype from "./skype_icon.png";
+import iconTelegram from "./telegram_icon.png";
+import iconViber from "./viber_icon.png";
+import iconVK from "./vk_icon.png";
+
+export { iconFacebook, iconSignal, iconSkype, iconTelegram, iconViber, iconVK };
