d02df2ca9c9e775c41824838d818020d6931ddd6
dumbling <romanskrt@yandex.ru>
2023-08-20 15:49:17 +0300

    feat: add redirect to MainPage if token is received for RegisterPage component

--- src/pages/register/RegisterPage.tsx
+++ src/pages/register/RegisterPage.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Header } from "../../shared/ui/Header";
 import { RegistrationForm } from "../../widgets/registration-form";
+import { customerTokenCache } from "../../shared/api";
+import { Paths } from "../../shared/constants/paths";
+import { useNavigate } from "react-router-dom";
 
 export function RegisterPage() {
+  const cachedToken = customerTokenCache.get().token;
+  const navigate = useNavigate();
+  useEffect((): void => {
+    if (cachedToken !== "") {
+      navigate(Paths.Main);
+    }
+  }, [cachedToken]);
   return (
     <div>
       <Header>Register page</Header>
