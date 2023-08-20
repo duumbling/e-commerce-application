146b13c6955e3a4e01eb1ed0e59186f67ddd66b1
dumbling <romanskrt@yandex.ru>
2023-08-20 14:10:40 +0300

    feat: add redirect to MainPage if token is received

--- src/pages/login/ui/LoginPage.tsx
+++ src/pages/login/ui/LoginPage.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Header } from "../../../shared/ui/Header";
 import { LoginForm } from "../../../widgets/login-form";
 import { Box, Grid } from "@mui/material";
 import { gridContainerProps, gridItemProps, gridHeaderProps } from "./style";
+import { useNavigate } from "react-router-dom";
+import { Paths } from "../../../shared/constants/paths";
+import { customerTokenCache } from "../../../shared/api/tokens";
 
 export function LoginPage() {
+  const cachedToken = customerTokenCache.get().token;
+  const navigate = useNavigate();
+  useEffect((): void => {
+    if (cachedToken !== "") {
+      navigate(Paths.Main);
+    }
+  }, [cachedToken]);
   return (
     <Box>
       <Grid {...gridContainerProps}>
