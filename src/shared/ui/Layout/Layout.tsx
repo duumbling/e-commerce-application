38eb7cf994997a796679f297f1acfc3bd01c0758
SkarabeyDM <mechanoid0415@gmail.com>
2023-08-21 23:01:14 +0300

    feat: add `Footer` to the `Layout`

--- src/shared/ui/Layout/Layout.tsx
+++ src/shared/ui/Layout/Layout.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Footer } from "../../../widgets/Footer";
 
 export function Layout() {
   return (
     <div className="wrapper">
       <Outlet />
-      <footer>--Footer--</footer>
+      <Footer />
     </div>
   );
 }
