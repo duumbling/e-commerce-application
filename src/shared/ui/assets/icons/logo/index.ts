1049373aaf58b3ffffc254ad4672514c2779c8cc
SkarabeyDM <mechanoid0415@gmail.com>
2023-08-21 09:49:31 +0300

    feat: add logo icons

---
+++ src/shared/ui/assets/icons/logo/index.ts
@@ -0,0 +1,3 @@
+export { ReactComponent as IconLogoBig } from "./logo_big.svg";
+export { ReactComponent as IconLogoSmall } from "./logo_small.svg";
+export { ReactComponent as IconLogoSmallBG } from "./logo_small_bg.svg";
