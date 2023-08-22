5b03b3ed084997f15b54e250f974ce206ff22260
SkarabeyDM <mechanoid0415@gmail.com>
2023-08-22 13:31:16 +0300

    feat: add logo

--- src/widgets/Footer/Footer.tsx
+++ src/widgets/Footer/Footer.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import { Grid } from "@mui/material";
+import { Logo } from "../../shared/ui/Logo";
 export function Footer() {
-  return <Grid component={"footer"}></Grid>;
+  return (
+    <Grid component={"footer"}>
+      <Grid item>
+        <Logo />
+      </Grid>
+    </Grid>
+  );
 }
