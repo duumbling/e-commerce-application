16ac52955c759cd5a994127fdda111700093ff71
roshik14 <fraust1337@gmail.com>
2023-08-23 14:59:02 +0700

    fix: replaced useNavigate with Navigate component from react router

--- src/pages/login/ui/LoginPage.tsx
+++ src/pages/login/ui/LoginPage.tsx
@@ -1,20 +1,19 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Header } from "../../../shared/ui/Header";
 import { LoginForm } from "../../../widgets/login-form";
 import { Box } from "@mui/material";
 import { AuthLayout } from "../../../shared/ui/AuthLayout";
 import { customerTokenCache } from "../../../shared/api";
-import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { Paths } from "../../../shared/constants/paths";
 
 export function LoginPage() {
-  const cachedToken = customerTokenCache.get().token;
-  const navigate = useNavigate();
-  useEffect((): void => {
-    if (cachedToken !== "") {
-      navigate(Paths.Main);
-    }
-  }, [cachedToken]);
+  const isAuthenticated = customerTokenCache.get().token !== "";
+
+  if (isAuthenticated) {
+    return <Navigate replace to={Paths.Main} />;
+  }
+
   return (
     <Box>
       <AuthLayout>
--- src/pages/register/ui/RegisterPage.tsx
+++ src/pages/register/ui/RegisterPage.tsx
@@ -3,8 +3,16 @@ import { Header } from "../../../shared/ui/Header";
 import { RegistrationForm } from "../../../widgets/registration-form";
 import { AuthLayout } from "../../../shared/ui/AuthLayout";
 import { Box } from "@mui/material";
+import { Navigate } from "react-router-dom";
+import { Paths } from "../../../shared/constants/paths";
+import { customerTokenCache } from "../../../shared/api";
 
 export function RegisterPage() {
+  const isAuthenticated = customerTokenCache.get().token !== "";
+
+  if (isAuthenticated) {
+    return <Navigate replace to={Paths.Main} />;
+  }
   return (
     <React.Fragment>
       <AuthLayout>
