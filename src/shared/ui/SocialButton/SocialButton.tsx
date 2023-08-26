cab2a9374d1f26baa30cdede3e27530107e5d6db
SkarabeyDM <mechanoid0415@gmail.com>
2023-08-26 23:32:26 +0300

    refactor: simplify `SocialButton`

--- src/shared/ui/SocialButton/SocialButton.tsx
+++ src/shared/ui/SocialButton/SocialButton.tsx
@@ -2,32 +2,26 @@ import React, { type ComponentProps } from "react";
 import { CustomButton } from "../CustomButton";
 
 import { style } from "./style";
-import { resolveSx } from "../../lib/helpers/resolveSx";
+import { Link } from "../Link";
+import { type Paths } from "../../constants/paths";
+import { bgIconCenter, resolveSx } from "../../lib/helpers/styles";
 
 export type SocialButtonProps = ComponentProps<typeof CustomButton> & {
-  text?: string;
+  href: Paths | string;
   icon?: string;
-  href: string;
 };
 
 export function SocialButton({
-  href,
   icon = "",
-  text = "",
   sx,
   ...otherProps
 }: SocialButtonProps) {
-  const sxStyle = resolveSx(
-    style,
-    {
-      background: `url(${icon}) no-repeat center`,
-    },
-    sx,
-  );
-
   return (
-    <CustomButton variant="outlined" sx={sxStyle} {...otherProps}>
-      {text}
-    </CustomButton>
+    <CustomButton
+      component={Link}
+      variant="outlined"
+      sx={resolveSx(style, bgIconCenter(icon), sx)}
+      {...otherProps}
+    />
   );
 }
