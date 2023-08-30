d219e8fb2819f775badb4641506d4255b736adbc
roshik14 <fraust1337@gmail.com>
2023-08-30 14:30:33 +0700

    fix: remove request for categories

--- src/pages/catalog/ui/CatalogPage.tsx
+++ src/pages/catalog/ui/CatalogPage.tsx
@@ -1,28 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { ProductCardsView } from "../../../features/ProductCardsView";
 import { Header } from "../../../shared/ui/Header";
-import { apiRoot } from "../../../shared/api/apiRoot";
 import { Box, Container, Grid } from "@mui/material";
 import { ProductsSortSelect } from "../../../entities/products-sort-select";
-import { useAppSelector } from "../../../shared/model/hooks";
 import { ProductsFilter } from "../../../entities/products-filter/ui/ProductsFilter";
 
 export function CatalogPage() {
-  const [categoryId, setCategoryId] = useState("");
-  const sortOption = useAppSelector((state) => state.sortProductsReducer);
-
-  useEffect(() => {
-    void (async () => {
-      const { body } = await apiRoot().categories().get().execute();
-      const categories = body.results.filter(
-        (value) => value.key === "sneakers",
-      );
-      if (categories.length >= 0) {
-        setCategoryId(categories[0].id);
-      }
-    })();
-  }, []);
-
   return (
     <>
       <Header>Каталог</Header>
@@ -34,10 +17,7 @@ export function CatalogPage() {
           </Grid>
           <Grid item>
             <Box maxWidth={784} margin="auto">
-              <ProductCardsView
-                categoryId={categoryId}
-                sort={sortOption.value}
-              />
+              <ProductCardsView categoryId="" />
             </Box>
           </Grid>
         </Grid>
