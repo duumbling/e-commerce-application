dae85df0bf9415b183ea7c07a8846135e0a585c0
roshik14 <fraust1337@gmail.com>
2023-09-01 15:36:38 +0700

    feat: add fontSize style for links

--- src/features/Breadcrumbs/ui/Breadcrumbs.tsx
+++ src/features/Breadcrumbs/ui/Breadcrumbs.tsx
@@ -4,6 +4,7 @@ import useBreadcrumbs from "use-react-router-breadcrumbs";
 import { breadcrumbs } from "../model/model";
 import { Link } from "../../../shared/ui/Link";
 import { ThemeColors } from "../../../shared/constants/colors";
+import { MOBILE_MEDIA } from "../../../shared/constants/mediaQuery";
 
 export function Breadcrumbs() {
   const breadcrumbsData = useBreadcrumbs(breadcrumbs);
@@ -15,6 +16,13 @@ export function Breadcrumbs() {
           key={value.key}
           href={value.match.pathname}
           color={ThemeColors.GREY}
+          sx={{
+            fontSize: 18,
+
+            [MOBILE_MEDIA]: {
+              fontSize: 13,
+            },
+          }}
         >
           {value.breadcrumb}
         </Link>
