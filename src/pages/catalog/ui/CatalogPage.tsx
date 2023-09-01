c77d0c7618e20da335b23bfed2e2b05be488583c
roshik14 <fraust1337@gmail.com>
2023-09-01 15:15:33 +0700

    fix: change grid style for adaptive

--- src/pages/catalog/ui/CatalogPage.tsx
+++ src/pages/catalog/ui/CatalogPage.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 import { CategoryProductsWidget } from "../../../widgets/category-products-widget/ui/CategoryProductsWidget";
+import { Grid } from "@mui/material";
+import { Breadcrumbs } from "../../../features/Breadcrumbs";
+import { ProductsSearch } from "../../../entities/products-search";
 
 export function CatalogPage() {
   return (
     <>
+      <Grid
+        container
+        justifyContent={{ md: "space-between", sm: "space-around" }}
+        marginTop={2}
+      >
+        <Grid item>
+          <Breadcrumbs />
+        </Grid>
+        <Grid item md={4} sm={4}>
+          <ProductsSearch />
+        </Grid>
+      </Grid>
       <CategoryProductsWidget />
     </>
   );
--- src/widgets/category-products-widget/ui/CategoryProductsWidget.tsx
+++ src/widgets/category-products-widget/ui/CategoryProductsWidget.tsx
@@ -1,32 +1,53 @@
 import React from "react";
-import { Box, Container, Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import { ProductsSortSelect } from "../../../entities/products-sort-select";
 import { ProductsFilter } from "../../../entities/products-filter";
 import { ProductCardsView } from "../../../features/ProductCardsView";
-import { ProductsSearch } from "../../../entities/products-search";
+import {
+  MOBILE_MEDIA,
+  TABLET_MEDIA,
+} from "../../../shared/constants/mediaQuery";
 
 export function CategoryProductsWidget() {
   return (
-    <Container maxWidth="lg">
-      <ProductsSearch />
-      <Grid container justifyContent="space-between" sx={{ marginTop: 3 }}>
-        <Grid item sm={12} md={7}>
-          <Typography variant="h3" color="primary">
-            Текущая категория
-          </Typography>
-        </Grid>
-        <Grid item sm={1} md={5}>
-          <ProductsSortSelect />
-        </Grid>
-        <Grid item sm={1} md={1}>
-          <ProductsFilter />
-        </Grid>
-        <Grid item sm={12} md={11}>
-          <Box maxWidth={784} margin="auto">
-            <ProductCardsView />
-          </Box>
-        </Grid>
+    <Grid
+      container
+      justifyContent={{
+        md: "center",
+        sm: "space-between",
+        xs: "space-between",
+      }}
+      sx={{ marginTop: 3 }}
+    >
+      <Grid item xs={12} sm={12} md={8}>
+        <Typography
+          variant="h3"
+          color="primary"
+          sx={{
+            fontSize: 40,
+            [TABLET_MEDIA]: {
+              fontSize: 30,
+              textAlign: "center",
+            },
+            [MOBILE_MEDIA]: {
+              fontSize: 25,
+            },
+          }}
+        >
+          Текущая категория
+        </Typography>
       </Grid>
-    </Container>
+      <Grid item sm={6} md={4} marginTop={{ md: 0, sm: 5, xs: 5 }}>
+        <ProductsSortSelect />
+      </Grid>
+      <Grid item sm={1} md={2} marginTop={{ md: 8, sm: 5, xs: 5 }}>
+        <ProductsFilter />
+      </Grid>
+      <Grid item sm={12} md={10} marginTop={7}>
+        <Box maxWidth={784} margin="auto">
+          <ProductCardsView />
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
