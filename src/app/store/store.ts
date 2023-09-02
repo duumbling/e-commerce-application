f1297400346cbca7a3fdc35bc3693e6ea782cb4b
roshik14 <fraust1337@gmail.com>
2023-09-02 08:56:39 +0700

    feat: add reducer to store

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import { addressReducer } from "../../features/AddressForm";
 import { productsFilterReducer } from "../../entities/products-filter";
 import { searchKeywordsReducer } from "../../entities/products-search";
+import { categoriesReducer } from "../../entities/category";
 
 export const store = configureStore({
   reducer: {
     addressReducer,
     productsFilterReducer,
     searchKeywordsReducer,
+    categoriesReducer,
   },
 });
