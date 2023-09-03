5d4ea3dc559d545a5f550e2b1df9d286ca6691e8
dumbling <romanskrt@yandex.ru>
2023-09-03 15:48:16 +0300

    feat: add index.ts file

---
+++ src/widgets/profile/index.ts
@@ -0,0 +1,4 @@
+export { CustomerInformationForm } from "./CustomerInformationForm/ui/CustomerInformationForm";
+export { CustomerAddressForm } from "./CustomerAddressForm/ui/CustomerAddressForm";
+export { AddressAccordion } from "./address-accordion/AddressAccordion";
+export type { AddressFormValues } from "./CustomerAddressForm/model/types";
