55746ca7a96d616bd555e17ca3de7ee68a30a08b
roshik14 <fraust1337@gmail.com>
2023-09-04 16:09:55 +0700

    feat: add header usage

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { Header } from "../../widgets/Header";
 
 export function MainPage() {
   return (
     <div>
+      <Header />
       <div>Главная страница</div>
     </div>
   );
