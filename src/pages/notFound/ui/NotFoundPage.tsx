72e330d98ce4a83e0841bbfeb52982fe08f8ced6
dumbling <romanskrt@yandex.ru>
2023-09-07 11:15:21 +0300

    feat: add header to NotFound page

--- src/pages/notFound/ui/NotFoundPage.tsx
+++ src/pages/notFound/ui/NotFoundPage.tsx
@@ -9,32 +9,36 @@ import {
 } from "./style";
 import { CustomButton } from "../../../shared/ui/CustomButton";
 import { useNavigate } from "react-router-dom";
+import { Header } from "../../../widgets/Header";
 
 export function NotFoundPage() {
   const navigate = useNavigate();
   return (
-    <Box marginTop="10%">
-      <Grid {...gridContainerProps}>
-        <Grid {...gridItemProps}>
-          <Typography variant="h1" component="p" {...firstLineStyle}>
-            404
-          </Typography>
-          <Typography variant="h3" component="p" {...secondLineStyle}>
-            Кажется, вы заблудились..
-          </Typography>
-          <Box>
-            <CustomButton
-              type="submit"
-              sx={goBackButtonStyle}
-              onClick={(): void => {
-                navigate(-1);
-              }}
-            >
-              Назад
-            </CustomButton>
-          </Box>
+    <div>
+      <Header />
+      <Box marginTop="10%">
+        <Grid {...gridContainerProps}>
+          <Grid {...gridItemProps}>
+            <Typography variant="h1" component="p" {...firstLineStyle}>
+              404
+            </Typography>
+            <Typography variant="h3" component="p" {...secondLineStyle}>
+              Кажется, вы заблудились..
+            </Typography>
+            <Box>
+              <CustomButton
+                type="submit"
+                sx={goBackButtonStyle}
+                onClick={(): void => {
+                  navigate(-1);
+                }}
+              >
+                Назад
+              </CustomButton>
+            </Box>
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </div>
   );
 }
