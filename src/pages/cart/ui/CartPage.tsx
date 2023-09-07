c0f44f216a43d4c07771ab673da7a1ec5aec5c2a
roshik14 <fraust1337@gmail.com>
2023-09-07 09:44:06 +0700

    refactor: rename cart items list component

--- src/pages/cart/ui/CartPage.tsx
+++ src/pages/cart/ui/CartPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Header } from "../../../widgets/Header";
-import { CartItemsList } from "../../../widgets/cart-items-list/ui/CartItemsWidget";
+import { CartItemsList } from "../../../widgets/cart-items-list";
 
 export function CartPage() {
   return (
