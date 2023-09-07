46138c456004283abbeec51a41160d2af1002038
roshik14 <fraust1337@gmail.com>
2023-09-07 13:09:21 +0700

    feat: add HeaderCartButton component

---
+++ src/widgets/Header/ui/HeaderCartButton/HeaderCartButton.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { IconButton, Badge, type IconButtonProps } from "@mui/material";
+import { ThemeColors } from "../../../../shared/constants/colors";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { useNavigate } from "react-router-dom";
+import { Paths } from "../../../../shared/constants/paths";
+
+export function HeaderCartButton({ onClick }: IconButtonProps) {
+  const navigate = useNavigate();
+
+  const handleClick = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    navigate(Paths.Cart);
+    if (onClick !== undefined) {
+      onClick(event);
+    }
+  };
+
+  return (
+    <IconButton size="large" onClick={handleClick}>
+      <Badge badgeContent={1} color={ThemeColors.PRIMARY}>
+        <ShoppingCartOutlinedIcon />
+      </Badge>
+    </IconButton>
+  );
+}
