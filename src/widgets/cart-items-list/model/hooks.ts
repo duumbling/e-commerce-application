dde5714c111ede99b8c3df30e710a0ed00e97045
roshik14 <fraust1337@gmail.com>
2023-09-07 19:09:12 +0700

    feat: add dispatch total price on load cart

--- src/widgets/cart-items-list/model/hooks.ts
+++ src/widgets/cart-items-list/model/hooks.ts
@@ -2,7 +2,9 @@ import { useEffect, useState } from "react";
 import { getCurrentCart } from "../../../entities/cart/api/cart";
 import { getCartProductData } from "../lib/helpers";
 import type { CartItemData } from "../../../features/CartItemView";
-import { useAppSelector } from "../../../shared/model/hooks";
+import { useAppDispatch, useAppSelector } from "../../../shared/model/hooks";
+import { cartSlice } from "../../../entities/cart";
+import { getPriceValue } from "../../../shared/api/product";
 
 export function useFetchCartItems() {
   const [isLoading, setIsLoading] = useState(false);
@@ -11,14 +13,19 @@ export function useFetchCartItems() {
 
   const cartState = useAppSelector((state) => state.cartReducer);
 
+  const { updateTotalPrice } = cartSlice.actions;
+
+  const dispatch = useAppDispatch();
+
   useEffect(() => {
     void (async () => {
-      setIsLoading(true);
+      // setIsLoading(true);
 
       try {
         const cart = await getCurrentCart();
         const items = cart.lineItems.map(getCartProductData);
         setProducts(items);
+        dispatch(updateTotalPrice(getPriceValue(cart.totalPrice)));
       } catch {
         // do nothing
       }
