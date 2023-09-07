f70e2301dfb1ad7f248d51793f1ea74b06c3d02c
dumbling <romanskrt@yandex.ru>
2023-09-07 16:21:58 +0300

    feat: add AbouUsItem component

---
+++ src/features/AboutUsItem/ui/AboutUsItem.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Avatar, Box, Grid, Typography } from "@mui/material";
+import { avatarStyle, bioStyle, rootStyle } from "./style";
+
+interface AboutUsItemProps {
+  image: string;
+  bio: string;
+  link: string;
+}
+
+export function AboutUsItem({ image, bio, link }: AboutUsItemProps) {
+  const words = bio.split(" ");
+  const firstWord = words[0];
+  const otherWords = words.slice(1).join(" ");
+  return (
+    <Box sx={rootStyle}>
+      <Grid>
+        <Avatar alt="Team member" src={image} sx={{ ...avatarStyle }} />
+        <Box>
+          <Typography variant="body1" sx={{ ...bioStyle }}>
+            <a href={link}>{firstWord}</a> {otherWords}
+          </Typography>
+        </Box>
+      </Grid>
+    </Box>
+  );
+}
