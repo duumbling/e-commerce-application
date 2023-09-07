ada7f794ae4d7412b5acf89fbec86518a2c9eaff
roshik14 <fraust1337@gmail.com>
2023-09-07 19:08:36 +0700

    feat: add order summary component usage

--- src/pages/cart/ui/CartPage.tsx
+++ src/pages/cart/ui/CartPage.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Header } from "../../../widgets/Header";
 import { CartItemsList } from "../../../widgets/cart-items-list";
+import { OrderSummary } from "../../../features/OrderSummary";
+import { Box } from "@mui/material";
 
 export function CartPage() {
   return (
     <div>
       <Header />
       <CartItemsList />
+      <Box marginTop={5}>
+        <OrderSummary />
+      </Box>
     </div>
   );
 }
