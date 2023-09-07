a52511012da93a8327f6c8ed046d0b6f6147f0f1
roshik14 <fraust1337@gmail.com>
2023-09-07 12:41:23 +0700

    feat: add empty cart message and link to catalog page

--- src/widgets/cart-items-list/ui/CartItemsList.tsx
+++ src/widgets/cart-items-list/ui/CartItemsList.tsx
@@ -1,14 +1,48 @@
 import React from "react";
-import { Backdrop, CircularProgress, Stack } from "@mui/material";
+import {
+  Backdrop,
+  CircularProgress,
+  Grid,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { useFetchCartItems } from "../model/hooks";
 import { CartItemView } from "../../../features/CartItemView";
 import { ThemeColors } from "../../../shared/constants/colors";
+import { Link } from "../../../shared/ui/Link";
+import { Paths } from "../../../shared/constants/paths";
 
 export function CartItemsList() {
   const { isLoading, products } = useFetchCartItems();
 
   return (
     <>
+      {!isLoading && products.length === 0 && (
+        <Grid
+          container
+          justifyContent="center"
+          marginTop={5}
+          textAlign="center"
+        >
+          <Grid item>
+            <Stack spacing={4}>
+              <Typography fontSize={{ md: 40, sm: 35, xs: 30 }}>
+                Корзина пуста
+              </Typography>
+              <Link
+                href={Paths.Catalog}
+                color={ThemeColors.PRIMARY}
+                sx={{
+                  marginTop: 8,
+                  fontSize: { md: 30, sm: 25, xs: 20 },
+                }}
+              >
+                В каталог
+              </Link>
+            </Stack>
+          </Grid>
+        </Grid>
+      )}
       <Stack spacing={2}>
         {products.map(
           ({ title, image, price, discountPrice, id, color, size }) => (
