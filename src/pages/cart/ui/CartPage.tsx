1ca562485ebe147455d5a4a9c2f3b7f67e26cc9b
roshik14 <fraust1337@gmail.com>
2023-09-09 15:09:53 +0700

    feat: add usage component

--- src/pages/cart/ui/CartPage.tsx
+++ src/pages/cart/ui/CartPage.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Header } from "../../../widgets/Header";
 import { CartProductsList } from "../../../widgets/cart-products-list";
+import { DiscountCodeField } from "../../../features/DiscountCodeField";
 
 export function CartPage() {
   return (
     <div>
       <Header />
       <CartProductsList />
+      <DiscountCodeField marginTop={3} />
     </div>
   );
 }
