029e227a1268ca2d1c08b47c90fdcc1dd1bd77ea
roshik14 <fraust1337@gmail.com>
2023-09-10 07:18:54 +0700

    feat: add redux thunk to load cart data

--- src/entities/cart/model/slice.ts
+++ src/entities/cart/model/slice.ts
@@ -1,16 +1,32 @@
-import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import {
+  createAsyncThunk,
+  createSlice,
+  type PayloadAction,
+} from "@reduxjs/toolkit";
+import { getCurrentCart } from "../api/cart";
+import { getPriceValue } from "../../../shared/api/product";
 
 interface CartState {
-  itemsCount: number;
   totalPrice: number;
   ids: string[];
 }
 
 const initialState: CartState = {
-  itemsCount: 0,
   totalPrice: 0,
   ids: [],
 };
+
+export const loadCartData = createAsyncThunk(
+  "cart",
+  async (): Promise<CartState> => {
+    const cart = await getCurrentCart();
+    const productIds = cart.lineItems.map((item) => item.productId);
+    return {
+      totalPrice: getPriceValue(cart.totalPrice),
+      ids: productIds,
+    };
+  },
+);
 
 export const cartSlice = createSlice({
   name: "cart",
@@ -26,6 +42,12 @@ export const cartSlice = createSlice({
     updateTotalPrice(state, { payload }: PayloadAction<number>) {
       state.totalPrice = payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(loadCartData.fulfilled, (state, { payload }) => {
+      state.ids = payload.ids;
+      state.totalPrice = payload.totalPrice;
+    });
   },
 });
 
