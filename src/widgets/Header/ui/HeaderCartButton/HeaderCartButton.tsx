e9b93393598ffcf0939a50edb4fb88818cedf11a
roshik14 <fraust1337@gmail.com>
2023-09-10 07:20:52 +0700

    feat: add counter from cart state

--- src/widgets/Header/ui/HeaderCartButton/HeaderCartButton.tsx
+++ src/widgets/Header/ui/HeaderCartButton/HeaderCartButton.tsx
@@ -4,9 +4,12 @@ import { ThemeColors } from "../../../../shared/constants/colors";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import { useNavigate } from "react-router-dom";
 import { Paths } from "../../../../shared/constants/paths";
+import { useAppSelector } from "../../../../shared/model/hooks";
 
 export function HeaderCartButton({ onClick, ...buttonProps }: IconButtonProps) {
   const navigate = useNavigate();
+
+  const { ids } = useAppSelector((state) => state.cartReducer);
 
   const handleClick = (
     event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
@@ -19,7 +22,7 @@ export function HeaderCartButton({ onClick, ...buttonProps }: IconButtonProps) {
 
   return (
     <IconButton size="large" {...buttonProps} onClick={handleClick}>
-      <Badge badgeContent={1} color={ThemeColors.PRIMARY}>
+      <Badge badgeContent={ids.length} color={ThemeColors.PRIMARY}>
         <ShoppingCartOutlinedIcon />
       </Badge>
     </IconButton>
