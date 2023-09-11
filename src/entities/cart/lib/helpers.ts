2132e92b3a95e80c5bf72c4967ab26f425e0dd07
roshik14 <fraust1337@gmail.com>
2023-09-11 17:59:17 +0700

    feat: add functions to get total price and discount price

--- src/entities/cart/lib/helpers.ts
+++ src/entities/cart/lib/helpers.ts
@@ -1,6 +1,26 @@
-import type { LineItem } from "@commercetools/platform-sdk";
+import type { Cart, LineItem } from "@commercetools/platform-sdk";
+import { getDiscountPrice, getPriceValue } from "../../../shared/api/product";
 
 export const getCurrentLineItem = (
   items: LineItem[],
   productId: string,
 ): LineItem => items.filter((item) => item.productId === productId)[0];
+
+export const getCartTotalPrice = (cart: Cart): number => {
+  return cart.lineItems.reduce(
+    (acc, curr) => acc + getPriceValue(curr.price.value) * curr.quantity,
+    0,
+  );
+};
+
+export const getCartDiscountPrice = (cart: Cart): number => {
+  return cart.lineItems.reduce((acc, curr) => {
+    const discountedPrice =
+      curr.discountedPricePerQuantity.length > 0
+        ? getPriceValue(
+            curr.discountedPricePerQuantity[0].discountedPrice.value,
+          )
+        : getDiscountPrice(curr.price);
+    return acc + (discountedPrice ?? 0) * curr.quantity;
+  }, 0);
+};
