ac875a4a76554d19e3c13dedbbd28d6cd6425aed
roshik14 <fraust1337@gmail.com>
2023-09-11 18:02:09 +0700

    fix: change model of cart products list

--- src/widgets/cart-products-list/lib/helpers.ts
+++ src/widgets/cart-products-list/lib/helpers.ts
@@ -1,11 +1,21 @@
-import type { LineItem } from "@commercetools/platform-sdk";
+import type {
+  DiscountedLineItemPriceForQuantity,
+  LineItem,
+} from "@commercetools/platform-sdk";
 import { getDiscountPrice, getPriceValue } from "../../../shared/api/product";
 import type { CartProductData } from "../../../features/CartProductView";
+
+const getCartProductDiscountedPrice = (
+  prices: DiscountedLineItemPriceForQuantity[],
+): number | undefined => {
+  return getPriceValue(prices[0].discountedPrice.value);
+};
 
 export const getCartProductData = ({
   productId,
   name,
   price,
+  discountedPricePerQuantity,
   variant,
   custom,
   quantity,
@@ -16,7 +26,10 @@ export const getCartProductData = ({
     title: name["ru-RU"],
     image: variant.images !== undefined ? variant.images[0].url : "",
     price: getPriceValue(price.value),
-    discountPrice: getDiscountPrice(price),
+    discountPrice:
+      discountedPricePerQuantity.length > 0
+        ? getCartProductDiscountedPrice(discountedPricePerQuantity)
+        : getDiscountPrice(price),
     totalPrice: getPriceValue(totalPrice),
     color: custom?.fields.color,
     size: custom?.fields.size,
