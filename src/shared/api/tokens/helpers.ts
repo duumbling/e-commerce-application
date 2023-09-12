3fdba8342d82305b8a5bf110bc9fec465bfafd8d
SkarabeyDM <mechanoid0415@gmail.com>
2023-09-12 15:23:47 +0300

    feat: add token helpers

--- src/shared/api/tokens/helpers.ts
+++ src/shared/api/tokens/helpers.ts
@@ -1,3 +1,4 @@
+import { type TokenStore } from "@commercetools/sdk-client-v2";
 import { customerTokenCache } from "./tokens";
 
 export const resetCustomerTokenCache = (): void => {
@@ -6,6 +7,42 @@ export const resetCustomerTokenCache = (): void => {
     expirationTime: 0,
     refreshToken: "",
   });
+};
+
+export const setCookie = (
+  key: string,
+  value: string,
+  expirationTime: number,
+) => {
+  document.cookie = `${key}=${value}; expires=${new Date(
+    expirationTime,
+  ).toUTCString()}`;
+};
+
+export const getCookie = (key: string) => {
+  let output: string | null = null;
+
+  const regex = new RegExp(`${key}=(.*?)(?:;|$)`, "g");
+  const cookieItem = regex.exec(document.cookie);
+
+  if (cookieItem !== null) {
+    output = cookieItem[1];
+  }
+
+  return output;
+};
+
+export const saveTokenStore = (key: string, cache: TokenStore) => {
+  setCookie(key, JSON.stringify(cache), cache.expirationTime);
+};
+
+export const loadTokenStore = (key: string): TokenStore | null => {
+  const cookieItem = getCookie(key);
+  if (cookieItem !== null) {
+    return JSON.parse(cookieItem);
+  }
+
+  return cookieItem;
 };
 
 export const isUserAuthenticated = () => customerTokenCache.get().token !== "";
