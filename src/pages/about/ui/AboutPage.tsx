154edcafd33d335a2a08fa674f00f889978898ec
roshik14 <fraust1337@gmail.com>
2023-09-15 14:06:11 +0700

    feat: add link to rs school

--- src/pages/about/ui/AboutPage.tsx
+++ src/pages/about/ui/AboutPage.tsx
@@ -4,13 +4,15 @@ import { AboutUsItem } from "../../../features/AboutUsItem/ui/AboutUsItem";
 import JamalImage from "../../../shared/assets/developers-bio/Jamal/JamalImage.jpg";
 import RashidImage from "../../../shared/assets/developers-bio/Rashid/man.png";
 import RomanImage from "../../../shared/assets/developers-bio/Roman/man.png";
+import CourseImage from "../../../shared/assets/images/rs_school.svg";
 import {
   RashidBio,
   RomanBio,
   JamalBio,
 } from "../../../shared/assets/developers-bio";
-import { Box } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import { rootStyle } from "./style";
+import { Link } from "../../../shared/ui/Link";
 
 const Links = {
   Jamal: "https://github.com/SkarabeyDM",
@@ -27,6 +29,13 @@ export function AboutPage() {
         <AboutUsItem bio={RashidBio} image={RashidImage} link={Links.Rashid} />
         <AboutUsItem bio={JamalBio} image={JamalImage} link={Links.Jamal} />
       </Box>
+      <Grid container justifyContent="center">
+        <Grid item>
+          <Link href="https://rs.school/">
+            <Box component="img" src={CourseImage} width={80} height={80} />
+          </Link>
+        </Grid>
+      </Grid>
     </>
   );
 }
